3698f6da4966eed0acbb9d85b003c3a4c2fe1999
root <system.te.xt@gmail.com>
2017-08-23 23:34:01 +0900

    New File

---
+++ web/index2.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>こちらはYES高須クリニックのデモサイトです。</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+こちらはLINE Messaging APIのデモサイトです。
+</body>
+</html>
